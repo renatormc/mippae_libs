3f02bb668a1336ad4e04dbc986b7b7c6f0b5380d
renatormc <renatomartinsrmc@gmail.com>
2021-06-04 14:49:11 -0300

    Normal

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mippae_libs',
     packages=find_packages(include=['mippae_libs']),
-    version='0.1.0',
+    version='0.1.1',
     description='Helpers for mippae project',
     author='Renato Martins',
     license='MIT',
